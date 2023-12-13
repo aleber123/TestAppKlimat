10912dbfcf396a70eb0c1e449902875dfc3d6e20
aleber123 <alexander.bergqvist@gmail.com>
2023-12-13 11:30:05 +0100

    Första commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './style.css';
+import Footer from './Components/footer';
+import Header from './Components/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
