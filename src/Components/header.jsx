120b2f871739d74a1048498c049576d479fc649d
aleber123 <alexander.bergqvist@gmail.com>
2023-12-13 13:37:52 +0100

    Header

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../Fonts.module.css'; // Import your CSS module
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: '#304742' }}>
+      <a className={`navbar-brand ${styles.poppins}`} href="#" style={{ color: '#FDFAF2' }}>
         Navbar
       </a>
       <button
@@ -21,43 +22,41 @@ const Header = () => {
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="#">
+            <a className={`nav-link ${styles.poppins}`} href="#" style={{ color: '#FDFAF2' }}>
               Home <span className="sr-only">(current)</span>
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className={`nav-link ${styles.poppins}`} href="#" style={{ color: '#FDFAF2' }}>
               Features
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className={`nav-link ${styles.poppins}`} href="#" style={{ color: '#FDFAF2' }}>
               Pricing
             </a>
           </li>
           <li className="nav-item dropdown">
             <a
-              className="nav-link dropdown-toggle"
+              className={`nav-link dropdown-toggle ${styles.poppins}`}
               href="#"
               id="navbarDropdownMenuLink"
               role="button"
               data-toggle="dropdown"
               aria-haspopup="true"
               aria-expanded="false"
+              style={{ color: '#FDFAF2' }}
             >
               Dropdown link
             </a>
-            <div
-              className="dropdown-menu"
-              aria-labelledby="navbarDropdownMenuLink"
-            >
-              <a className="dropdown-item" href="#">
+            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a className={`dropdown-item ${styles.poppins}`} href="#" style={{ color: 'black' }}>
                 Action
               </a>
-              <a className="dropdown-item" href="#">
+              <a className={`dropdown-item ${styles.poppins}`} href="#" style={{ color: 'black' }}>
                 Another action
               </a>
-              <a className="dropdown-item" href="#">
+              <a className={`dropdown-item ${styles.poppins}`} href="#" style={{ color: 'black' }}>
                 Something else here
               </a>
             </div>
