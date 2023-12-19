e63d46c0224d4edaee5a44cde7c5ad47b269885a
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-19 17:37:05 +0100

    header update

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -3,45 +3,51 @@ import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../Fonts.module.css'; // Import your CSS module
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Header = () => {
+
+// ... (your imports)
+
+function Header() {
   return (
-    <nav className="navbar navbar-expand-lg " style={{ height:"12rem", backgroundColor: '#304742' }}>
-      <Link to="./" >
-      <img style={{height:"200px", width:"200px"}} className='p-2' src="https://i.ibb.co/vxM3hm5/logotyp1.png"alt="logotyp" border="0" />
-      </Link> 
-      <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNavDropdown ">
-        <ul className="navbar-nav ">
-          <li>
-            <Link className={`nav-link `} to="/quiz" style={{ color: '#FDFAF2' }} >
-            <p className={`${styles.openSans} ${styles.p} pt-4`}>Testa</p>
+    <div style={{ height: '10rem', backgroundColor: '#304742' }}>
+      <Navbar className='p-3' >
+        <Navbar.Brand>
+          <span className=''>
+            <Link to="./">
+              <img style={{ height: '130px', width: '130px' }} className='' src="https://i.ibb.co/hyVT6m5/S-4.png" alt="logotyp" border="0" />
             </Link>
-            </li>
-          <li className="nav-item">
+          </span>
+        </Navbar.Brand>
+        <p className={`${styles.openSans} ${styles.p} p-2`} style={{ color: '#FDFAF2' }}>Klimatkompassen</p>
+        <Container>
+          <Nav className="d-flex justify-content-around w-100">
+            <Link className={`nav-link ${styles.poppins}`} to="/quiz" style={{ color: '#FDFAF2' }}>
+              <h4 className={`${styles.poppins} ${styles.bold} ${styles.h4} px-2`}>Quiz</h4>
+            </Link>
+
             <Link className={`nav-link ${styles.poppins}`} to="/forum" style={{ color: '#FDFAF2' }}>
-            <p className={`${styles.openSans} ${styles.p} pt-4`}>Forum</p>
+              <h4 className={`${styles.poppins} ${styles.bold} ${styles.h4}`}>Forum</h4>
             </Link>
-          </li>
-          <li className="nav-item">
+
             <Link className={`nav-link ${styles.poppins}`} to="/workshop" style={{ color: '#FDFAF2' }}>
-            <p className={`${styles.openSans} ${styles.p} pt-4`}>Workshop</p>
+              <h4 className={`${styles.poppins} ${styles.bold} ${styles.h4}`}>Workshop</h4>
             </Link>
-          </li>
-          <li className="nav-item">
+
             <Link className={`nav-link ${styles.poppins}`} to="/nyheter" style={{ color: '#FDFAF2' }}>
-            <p className={`${styles.openSans} ${styles.p} pt-4`}>Fakta</p>
+              <h4 className={`${styles.poppins} ${styles.bold} ${styles.h4}`}>Fakta</h4>
             </Link>
-          </li>
-          <li className="nav-item">
+
             <Link className={`nav-link ${styles.poppins}`} to="/kontakt" style={{ color: '#FDFAF2' }}>
-            <p className={`${styles.openSans} ${styles.p} pt-4`}>kontakt</p>
+              <h4 className={`${styles.poppins} ${styles.bold} ${styles.h4}`}>Kontakt</h4>
             </Link>
-          </li>
-          {/* ... Other navigation links */}
-        </ul>
-      </div>
-    </nav>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
   );
-};
+}
 
 export default Header;
