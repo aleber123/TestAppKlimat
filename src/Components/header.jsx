be45a5efde60c3bfe2ca0618d2f90ecc6d19c497
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-19 14:35:06 +0100

    header update

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -3,38 +3,38 @@ import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../Fonts.module.css'; // Import your CSS module
 
+
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: '#304742' }}>
-      <Link className={`navbar-brand ${styles.poppins}`} to="/" style={{ color: '#FDFAF2' }}>
-        Navbar
-      </Link>
-      {/* ... Other elements in the header */}
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className={`nav-link ${styles.poppins}`} to="/quiz" style={{ color: '#FDFAF2' }}>
-              Klimatquiz & utmaningar
+    <nav className="navbar navbar-expand-lg " style={{ height:"7rem", backgroundColor: '#304742' }}>
+      <Link to="./" >
+      <img style={{height:"100px", width:"100px"}} className='p-2' src="https://i.ibb.co/vxM3hm5/logotyp1.png"alt="logotyp" border="0" />
+      </Link> 
+      <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNavDropdown ">
+        <ul className="navbar-nav ">
+          <li>
+            <Link className={`nav-link `} to="/quiz" style={{ color: '#FDFAF2' }} >
+            <p className={`${styles.openSans} ${styles.p} pt-4`}>Testa</p>
             </Link>
-          </li>
+            </li>
           <li className="nav-item">
             <Link className={`nav-link ${styles.poppins}`} to="/forum" style={{ color: '#FDFAF2' }}>
-              Diskussionsforum
+            <p className={`${styles.openSans} ${styles.p} pt-4`}>Forum</p>
             </Link>
           </li>
           <li className="nav-item">
             <Link className={`nav-link ${styles.poppins}`} to="/workshop" style={{ color: '#FDFAF2' }}>
-              Workshop & Evenemang
+            <p className={`${styles.openSans} ${styles.p} pt-4`}>Workshop</p>
             </Link>
           </li>
           <li className="nav-item">
             <Link className={`nav-link ${styles.poppins}`} to="/nyheter" style={{ color: '#FDFAF2' }}>
-              Nyheter & artiklar
+            <p className={`${styles.openSans} ${styles.p} pt-4`}>Fakta</p>
             </Link>
           </li>
           <li className="nav-item">
             <Link className={`nav-link ${styles.poppins}`} to="/kontakt" style={{ color: '#FDFAF2' }}>
-              Kontakt
+            <p className={`${styles.openSans} ${styles.p} pt-4`}>kontakt</p>
             </Link>
           </li>
           {/* ... Other navigation links */}
