db5448755dc82c39eaf4c535559c719f1e2bacd2
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-20 13:19:06 +0100

    ny vy

---
+++ src/Vyer/OmWorkshopVy.jsx
@@ -0,0 +1,17 @@
+
+import MallEtt from "../Components/MallEtt";
+import Kalender from "../Components/Kalender";
+import SynkaHel from "../Components/SynkaHel";
+function OmWorkshopVy() {
+  return (
+<div>
+ <MallEtt />
+  <Kalender />
+  <SynkaHel />  
+
+
+</div> 
+  );
+}
+
+export default OmWorkshopVy;
