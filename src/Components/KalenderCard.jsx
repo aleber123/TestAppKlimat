cb5ac40553a975ae128a11606e64f194cfeb9d86
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-20 10:24:11 +0100

    uppdaterad

--- src/Components/KalenderCard.jsx
+++ src/Components/KalenderCard.jsx
@@ -1,17 +1,18 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import styles from '../Fonts.module.css'; // Import your CSS module
-
+import { getWorkshopInfo } from './WorkshopArray';
 
 
 
 function KalenderCard() {
-  return (
+  const WorkArray= getWorkshopInfo();
+  return WorkArray.map((work) =>  (
     <Card style={{ width: '15rem', backgroundColor:'FDFAF2' }}>
-      <Card.Img variant="top" src="https://img.freepik.com/free-photo/top-view-eco-friendly-cleaning-products-with-lemon_23-2148818433.jpg?w=1480&t=st=1702464969~exp=1702465569~hmac=9721e9a72b0135a1047b542957f09d0b0d2fb5d965cfc7d46d15f46e1e65f8d5" />
+      <Card.Img variant="top" src={work.picture} />
       <Card.Body>
        
-        <Card.Title  className={`${styles.openSans} ${styles.bold} ${styles.h3} text-center p-2`} >Ekologiska städprodukter</Card.Title>
+        <Card.Title  className={`${styles.openSans} ${styles.bold} ${styles.h3} text-center p-2`} >{work.Namn}</Card.Title>
         <div class="d-flex justify-content-center p-2">
         <Button style={{
                 width: '10rem',
@@ -21,7 +22,7 @@ function KalenderCard() {
         </div>
       </Card.Body>
     </Card>
-  );
+  ));
 }
 
 export default KalenderCard;
