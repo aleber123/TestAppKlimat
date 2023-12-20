9a46532ba0ac1212559822197329c4d764125a71
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-20 10:22:41 +0100

    lista kontakt

---
+++ src/Components/KontaktArray.jsx
@@ -0,0 +1,44 @@
+
+  
+  export const getkontaktInfo = () => {
+    return [
+        
+            {
+              id:"1",
+              picture:"https://img.freepik.com/free-photo/smiling-elderly-woman-holds-notebook-pen-isolated-olive-green-wall_141793-70433.jpg?w=1480&t=st=1703021190~exp=1703021790~hmac=f2883186789daaa2c7d93ece517d91b39ba72b72d61c553314e0d587749cd3c0",
+              Namn: "Vendela Björkbom",
+              yrke: "Workshop handledare",
+              mail: "Vendela@klimatkompassen.se"
+            },
+            {
+                id:"2",
+                picture:"https://img.freepik.com/free-photo/front-view-adults-studying-together_23-2150331658.jpg?w=740&t=st=1703021073~exp=1703021673~hmac=d0492974af91e2180004943b3d9507ab19159cd339381d917f76b15ef4540283",
+                Namn: "Jonas Berglund",
+                yrke: "Ekomoni ansvarig",
+                mail: "Jonas@klimatkompassen.se"
+            },
+            {
+                id:"3",
+                picture:"https://img.freepik.com/free-photo/portrait-wise-person_52683-100915.jpg?w=1480&t=st=1703025259~exp=1703025859~hmac=6ebf39073b02dd94a10f36f9b73830efc189a6555bd80cab47cc454464550177",
+                Namn: "Gunnar Länström",
+                yrke: "Workshop handledare",
+                mail: "Gunnar@klimatkompassen.se"
+            },
+            {
+                id:"4",
+                picture:"https://img.freepik.com/premium-photo/portrait-attractive-adult-woman-wearing-eyeglasses-holding-studying-books-pen-isolated-white-wall-studio_171337-103354.jpg?w=1480",
+                Namn: "Julia Widersjö",
+                yrke: "Ekonom assisent",
+                mail: "julia@klimatkompassen.se"
+            },
+            {
+                id:"5",
+                picture:"https://img.freepik.com/free-photo/medium-shot-elderly-woman-studying-classroom_23-2150312856.jpg?w=1480&t=st=1703021228~exp=1703021828~hmac=887397a17e709da5d9b81add7605e316c4e5d6c2f4ac0489aeac418b9d9aaa21 ",
+                Namn: "Ulrika Öhman",
+                yrke: "kommunikatör",
+                mail: "Ulrika@klimatkompassen.se"
+            },   
+          ];
+  };
+  
+  
