0c8d61a7309daf2fa5e6e37ab28288fa20d3d0c6
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-20 16:58:11 +0100

    ny

--- src/Vyer/OmWorkshopVy.jsx
+++ src/Vyer/OmWorkshopVy.jsx
@@ -2,13 +2,18 @@
 import MallEtt from "../Components/MallEtt";
 import Kalender from "../Components/Kalender";
 import SynkaHel from "../Components/SynkaHel";
+import WorkshopTidigare from "../Components/WorkshopTidigare";
+import OmWorkshop from "../Components/OmWorkshop";
+
+
 function OmWorkshopVy() {
   return (
 <div>
  <MallEtt />
   <Kalender />
-  <SynkaHel />  
-
+  <SynkaHel />
+   <WorkshopTidigare /> 
+    <OmWorkshop/>
 
 </div> 
   );
