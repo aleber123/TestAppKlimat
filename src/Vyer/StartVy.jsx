ad0ce03999b28e6076efb1f7f9a2dfd1ba209c04
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-20 10:47:11 +0100

    test

--- src/Vyer/StartVy.jsx
+++ src/Vyer/StartVy.jsx
@@ -1,8 +1,6 @@
-import MallEtt from '../Components/MallEtt';
-import styles from '../Fonts.module.css'; // Import your CSS module
-import WorkshopVad from '../Components/WorkshopVad';
+
 import HeaderBild from '../Components/HeaderBild';
-import Header from '../Components/header';
+
 function StartVy() {
   return (
 <div>
