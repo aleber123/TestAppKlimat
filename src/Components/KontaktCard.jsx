b813a8e4dcb02b789122531882e35ff2c838f310
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-20 10:25:29 +0100

    uppdaterad

--- src/Components/KontaktCard.jsx
+++ src/Components/KontaktCard.jsx
@@ -1,19 +1,26 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import styles from '../Fonts.module.css'; // Import your CSS module
+import { getkontaktInfo } from './KontaktArray';
 
 function KontaktCard() {
-  return (
-    <Card style={{ width: '15rem', backgroundColor:'FDFAF2' }}>
-      <Card.Img variant="top" src="https://img.freepik.com/free-photo/positive-human-reactions-feelings-emotions-charming-elegant-middle-aged-sixty-year-old-female-with-short-gray-hair-with-pleased-smile-her-eyes-full-happiness-joy_343059-2855.jpg?w=1480&t=st=1702488320~exp=1702488920~hmac=3fd5edb17de34223f20ead08306aabbe018cf11ed1c726a98f812eb3e6245938" />
+ const kontaktArray= getkontaktInfo();
+  return kontaktArray.map((kontakt) => (
+
+    <Card style={{ width: '16rem', height:"20rem", backgroundColor:'FDFAF2' }}>
+       <Card.Img
+        style={{ height: '10rem', width: 'auto', objectFit: 'cover' }}
+        variant="top"
+        src={kontakt.picture}
+      />
       <Card.Body>
-        <Card.Title><p className={`${styles.openSans} ${styles.p}`} >Johanna Fransson</p></Card.Title>
+        <Card.Title><p className={`${styles.openSans} ${styles.p} text-center`} >{kontakt.Namn}</p></Card.Title>
         <Card.Text>
-          <p className={`${styles.openSans} ${styles.p}`}>Johanna@mail.com</p>
+          <p className='text-center' >{kontakt.mail}</p>
         </Card.Text>
       </Card.Body>
     </Card>
-  );
+   ) );
 }
 
 export default KontaktCard;
