479ce6d2188c4993a5c4ab42efeb109293b58984
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-20 10:42:52 +0100

    testar

---
+++ src/Vyer/WorkshopVadVy.jsx
@@ -0,0 +1,17 @@
+import HeaderBild from '../Components/HeaderBild';
+import MallEtt from '../Components/MallEtt';
+import WorkshopVad from '../Components/WorkshopVad';
+function WorkshopVadVy() {
+  return (
+<div>
+    <HeaderBild/>
+    <MallEtt/>
+    <WorkshopVad />
+
+
+ 
+</div> 
+  );
+}
+
+export default WorkshopVadVy;
