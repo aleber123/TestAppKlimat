ed48ec8404d10234ce6e9b41efab1e72a289a557
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-20 16:56:31 +0100

    felstavad med korrekt

---
+++ src/Vyer/AmalaVy.jsx
@@ -0,0 +1,16 @@
+import AnmalanVad from "../Components/AnmalanVad";
+import AnmalFormular from "../Components/AnmalFormular";
+import Ledare from "../Components/Ledare";
+import MallEtt from "../Components/MallEtt";
+function AnmalaVy() {
+  return (
+<div>
+ <MallEtt />   
+ <AnmalanVad />
+<AnmalFormular /> 
+<Ledare />
+</div> 
+  );
+}
+
+export default AnmalaVy;
