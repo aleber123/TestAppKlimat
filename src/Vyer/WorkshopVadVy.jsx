09da60e727193cdee0489bb12b0146a5f85f87b7
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-20 12:00:45 +0100

    ny

--- src/Vyer/WorkshopVadVy.jsx
+++ src/Vyer/WorkshopVadVy.jsx
@@ -1,12 +1,15 @@
 import HeaderBild from '../Components/HeaderBild';
+import Ledare from '../Components/Ledare';
 import MallEtt from '../Components/MallEtt';
 import WorkshopVad from '../Components/WorkshopVad';
+
 function WorkshopVadVy() {
   return (
 <div>
     <HeaderBild/>
     <MallEtt/>
     <WorkshopVad />
+    <Ledare />
 
 
  
