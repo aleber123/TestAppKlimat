174fd1d1fb9f555515d9a1a24c9a5eeae626a3be
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-20 16:59:29 +0100

    ny

--- src/Components/KalenderCard.jsx
+++ src/Components/KalenderCard.jsx
@@ -8,17 +8,20 @@ import { getWorkshopInfo } from './WorkshopArray';
 function KalenderCard() {
   const WorkArray= getWorkshopInfo();
   return WorkArray.map((work) =>  (
+
     <Card style={{ width: '15rem', backgroundColor:'FDFAF2' }}>
-      <Card.Img variant="top" src={work.picture} />
+      <Card.Img variant="top" src={work.picture}  />
       <Card.Body>
        
-        <Card.Title  className={`${styles.openSans} ${styles.bold} ${styles.h3} text-center p-2`} >{work.Namn}</Card.Title>
+        <Card.Title style={{ height: '3rem', backgroundColor:'FDFAF2' }} className={`${styles.openSans} ${styles.bold} ${styles.h3} text-center p-2`} >{work.Namn}</Card.Title>
         <div class="d-flex justify-content-center p-2">
         <Button style={{
                 width: '10rem',
                 height: '3rem',
-                backgroundColor: '#BCD0A5',
-              }}><p className={`${styles.openSans} ${styles.p} 'd-flex justify-content-center' `} style ={{color:'#304742'}}>Till Workshop</p></Button>
+                marginTop: '2rem',
+                backgroundColor: '#567B5D',
+                
+              }}><p className={`${styles.openSans} ${styles.p} 'd-flex justify-content-center' `} style ={{color:'#FDFAF2'}}>Till Workshop</p></Button>
         </div>
       </Card.Body>
     </Card>
