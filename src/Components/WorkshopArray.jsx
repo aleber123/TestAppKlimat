7db129dcb58190ac042490ec2a7a872509b7046b
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-20 10:41:08 +0100

    array workshop

---
+++ src/Components/WorkshopArray.jsx
@@ -0,0 +1,36 @@
+export const getWorkshopInfo = () => {
+    return [
+    {
+      id:"1",
+      picture:"https://img.freepik.com/free-photo/portrait-biologist-scientist-white-coat-working-expertise-laboratory-looking-into-microscope-analyzing-organic-gmo-leaf_482257-2118.jpg?w=2000&t=st=1703022094~exp=1703022694~hmac=9dc984a047410ff4e9d92c124d9a0dfaec604c16508e17ff253b7a694006adef",
+      Namn: "Framtidens Mat",
+      
+      
+    },
+    {
+        id:"2",
+        picture:"https://img.freepik.com/premium-photo/mix-dried-fruits-wooden-table_211889-688.jpg?w=1480",
+        Namn: "Säsongsbetonade Ingredienser",
+       
+    },
+    {
+        id:"3",
+        picture:"https://img.freepik.com/free-photo/young-farmer-taking-care-his-business_329181-15958.jpg?w=1480&t=st=1703022274~exp=1703022874~hmac=6347065c16656e4cbd5389b3397b8c82b5a7e421a12dfae4841119bdc1caa7ba",
+        Namn: "Besök från Lokala Producenter",
+        
+    },
+    {
+        id:"4",
+        picture:"https://img.freepik.com/free-photo/person-holding-bowl-salad-made-dehydrated-onions_181624-23126.jpg?w=1480&t=st=1703022325~exp=1703022925~hmac=48ea2f87f9f77a472ef0698a4b6edd4c9f3f4e1918bb6cea36a3f95cb0b87c2d",
+        Namn: " Klimatpåverkan av Mat",
+    
+    },
+    {
+        id:"5",
+        picture:"https://img.freepik.com/free-photo/celebrating-with-sparklers-night_53876-18286.jpg?w=1480&t=st=1703022358~exp=1703022958~hmac=3dbd059816c1218232aa7b21aba953b491541c12d414d2146bc1d43d20109af0",
+        Namn: "Klimatsmart Nyårsmiddag",
+      
+    },   
+  ];
+}
+  export default kontaktInfo;
