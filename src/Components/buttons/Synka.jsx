a850aa7ab3e42231b246f03b0beec5d9452afd44
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-20 13:16:48 +0100

    knapp

---
+++ src/Components/buttons/Synka.jsx
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button';
+
+function Synka() {
+  return (
+    <div className="">
+      <Button variant="primary" size="lg" className='text-center 'style={{backgroundColor:'#BCD0A5', color:'#304742', height:"3rem", width:"12rem"}}>
+      <p>Synka alla event</p>
+      </Button>
+    </div>
+  );
+}
+
+export default Synka;
