c8a09a3c1c885a15ef6e16fa683e25d1bbc7e46b
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-22 16:54:20 +0100

    ihop

---
+++ src/Components/grafer/Co2Text.jsx
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Co2Chart from './Co2LineChart';
+
+function Co2Text() {
+  return (
+    <div className="" style={{backgroundColor:'#BCD0A5'}}>
+      
+     <Container style={{backgroundColor:'#FDFAF2'}} className='p-5'>
+     <h2 className='text-center p-2'>Koldioxidutsläpp</h2>
+        <Row>
+            <Col>
+            <Co2Chart />
+            </Col>
+            <Col>
+            <p >
+                Sveriges miljömål beskriver att de senaste åren - 2015 till 2022 - har utgjort de åtta varmaste åren som har registrerats. Huvudorsaken till den observerade uppvärmningen av jordens klimat är den förändring av luftens kemiska sammansättning som människan orsakar genom utsläpp av växthusgaser sveriges miljömål. Förutom av mänsklig verksamhet påverkas klimatet, i olika tidshorisonter, även av naturliga fenomen såsom vulkanutbrott, förändringar i solens aktivitet, förändringar i jordens rörelser runt solen och runt sin egen axel, samt interna variationer i klimatet exempelvis ”El Niño”-cyklerna
+            </p>
+            </Col>
+        </Row>
+
+    
+        </Container>   
+    
+    </div>
+  );
+}
+
+export default  Co2Text;
