e78ccc77baa07eebd44dca7f587c16a5e5cf858f
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-22 17:40:41 +0100

    havsniva

---
+++ src/Components/grafer/HavsNiva.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+const HavsNiva = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://my.api.mockaroo.com/sealevel.json?key=8eb9e6f0');
+        const result = await response.json();
+        console.log(result); // Logga result för att se API-svaret
+
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div style={{ backgroundColor: '#FDFAF2' }}>
+      <LineChart width={730} height={250} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="Time" />
+        <YAxis dataKey="GMSL" domain={['auto', 'auto']} />
+        <Line type="monotone" dataKey="GMSL" stroke="#304742" strokeWidth={2} dot={false} />
+      </LineChart>
+    </div>
+  );
+};
+
+export default HavsNiva;
