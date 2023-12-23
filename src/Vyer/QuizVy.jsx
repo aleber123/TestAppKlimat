48513e8e6353400284bc62cff2167f3bd82aef44
stinasigridlowisaberg <stinaberg_@hotmail.com>
2023-12-23 17:07:40 +0100

    ny

---
+++ src/Vyer/QuizVy.jsx
@@ -0,0 +1,13 @@
+
+import QuizHeader from "../Components/QuizHeader";
+import Quiz from "../Components/Quiz";
+function QuizVy() {
+    return (
+  <div>
+ <QuizHeader />
+ <Quiz />       
+  </div> 
+    );
+  }
+  
+  export default QuizVy;
